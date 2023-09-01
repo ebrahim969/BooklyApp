6a55c0ff8c4cd23b06debe722104408c95cf9e1b
ebrahim969 <ebrahimelnemr084@gmail.com>
2023-09-01 06:48:21 +0300

    implement fetch newest books

--- lib/features/home/data/repos/home_repo_implement.dart
+++ lib/features/home/data/repos/home_repo_implement.dart
@@ -1,18 +1,30 @@
+import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/core/errors/failuers.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class HomeReopImpl implements HomeRepo
-{
+class HomeReopImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeReopImpl(this.apiService);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint: "volumes?Sorting=newest &q=subject:programming");
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }    
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
     throw UnimplementedError();
   }
-  
 }
