7a89697a09e6d741348d9357b016769b8d1ecfb1
ebrahim969 <ebrahimelnemr084@gmail.com>
2023-09-01 20:13:43 +0300

    create fetch featured books

--- lib/features/home/data/repos/home_repo_implement.dart
+++ lib/features/home/data/repos/home_repo_implement.dart
@@ -13,7 +13,7 @@ class HomeReopImpl implements HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
     try {
       var data = await apiService.get(
-          endPoint: "volumes?Sorting=newest &q=subject:programming");
+          endPoint: "volumes?Filtering=free-ebooks&Sorting=newest &q=programming");
       List<BookModel> books = [];
       for (var item in data['items']) {
         books.add(BookModel.fromJson(item));
@@ -29,7 +29,21 @@ class HomeReopImpl implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async{
+      try {
+      var data = await apiService.get(
+          endPoint: "volumes?Filtering=free-ebooks&q=programming");
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }    
+      return right(books);
+    } catch (e) {
+      if(e is DioException)
+      {
+        return left(ServerFailure.fromDioError(e),);
+      }
+      return left(ServerFailure(e.toString()));
+    }
   }
 }
